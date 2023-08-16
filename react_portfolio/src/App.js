6e4c412310a6adea4e53a81dff483192487d39d2
Paul Nixon <paulnxn1@gmail.com>
2023-08-16 19:12:53 -0400

    About Page.jsx Updated

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,11 +1,13 @@
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
       <div>
         <Navbar />
         <Homepage />
+        <About />
 
       </div>
   );
--- react_portfolio/src/components/Homepage.jsx
+++ react_portfolio/src/components/Homepage.jsx
@@ -3,23 +3,23 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Homepage = () => {
 
- 
-
   return (
     <div name='home' className=' bg-[#2b5390]'>
 
         {/* Main content */}
 
-        <div className='max-w-[1000px] mx-auto px-20 flex flex-col justify-center h-full'>
+        <div className='max-w-[750px] mx-auto px-20 flex flex-col justify-center h-full'>
             
-            <h1 className='text-white font-bold text-7xl sm:text-7xl'>Hi! I'm Paul Nixon</h1>
+            <h1 className='text-white font-bold text-7xl sm:text-7xl'>Paul Nixon</h1>
             <h2 className='text-white font-bold'>Full Stack Developer</h2>
-            <p className='text-white font-bold'>Currently in a Boot Camp for UPENN.  The goal is to master full stack web development</p>
-        </div>
-        <br></br>
+            <p className='text-white font-bold'>Currently in a Boot Camp for UPENN.</p>
+            <button className='text-white group border-2 px-1 py-3 my-3 flex justify-center hover:bg-[#8BB7CC] hover:border-black'> View Projects  <span className='group-hover:rotate-90 duration-8000'>
 
-        <div>
-            <button className='max-w-[1000px] mx-auto px-6 flex flex-col justify-center h-full'> View Projects <HiArrowNarrowRight/> </button>
+            <HiArrowNarrowRight className='ml-4'/> 
+                
+            </span>
+            
+            </button>
         </div>
 
 
